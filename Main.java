5c2b2f0fddf4b14e98de5f7daa7b693b5629a3b0
naoki kanda <naoki.kanda@mix-lab.net>
2024-06-18 02:45:56 +0900

    commit Main.java Player.java

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
-    System.out.println("Hello SEMonster");
+    System.out.println("SEMonster GAME\n");
+    System.out.println("Show a new monster");
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
   }
 }
