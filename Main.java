5655761fad1453c4aacd8ac2aec8259f9a073ed4
naoki kanda <naoki.kanda@mix-lab.net>
2024-06-18 03:13:24 +0900

    Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMonster GAME\n");
-    System.out.println("Show a new monster");
+    System.out.println("SEMonster GAME");
     Player player = new Player();
     player.drawMonsters();
     player.showDeck();
